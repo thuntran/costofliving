7985d53e9ffa4fcf40c80d500c4cd51d2765a55e
Nina Hernandez <nhernandez@smith.edu>
2022-11-20 23:45:49 -0500

    Unit test for grossSalaryMutliple function

--- tests/testthat/test-grossSalaryMultiple.R
+++ tests/testthat/test-grossSalaryMultiple.R
@@ -1,4 +1,4 @@
-test_that("Testing grossSalaryMultiple", {
+test_that("Testing grossSalaryMultiple output ", {
   expect <- data.frame(state = ("Massachusetts"),
                    stateAbbr=("MA"),
                    stateTaxRate=(6.25),
