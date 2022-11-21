cff3aab57226c52de655f383d95c08e31635de95
Thu Tran <tntran@smith.edu>
2022-11-20 22:14:27 -0500

    Add examples of grossSalaryMultiple() in readme, fix export issues

--- R/salaryFunctions.R
+++ R/salaryFunctions.R
@@ -1,18 +1,60 @@
-#' A gross salary calculator based on user's input of states
+# Define global variables
 #'
-#' There are two functions to help calculate gross salaries earning minimum wage for states
-#' gross salary is an added column to existing data set
+#' @import utils
+globalVariables(c("minimumWage", "stateAbbr", "us_cost_of_living"))
+
+#' A function that calculates the annual gross salary of a single person earning
+#' minimum wage at a specific state, given a state as input.
 #'
-#' @import tidyverse
+#' grossSalary is an added column to the existing us_cost_of_living data set.
+#' This serves as a helper function to the following grossSalaryMultiple()
+#' function.
+#'
 #' @import dplyr
 #' @importFrom magrittr %>%
+#'
+#' @param state A state name, abbreviated (e.g. MA)
+#'
+#' @return A tibble of taxes, average living costs, minimum wage, and gross
+#' salaries earning minimum wage per state
+#'
+#' @export
+#'
+#' @examples
+#' grossSalary("MA")
+#' grossSalary("TX")
+#'
+
+grossSalary <- function(state) {
+  # checks if user inputs a valid state abbreviation
+  if (state %in% us_cost_of_living$stateAbbr) {
+    us_cost_of_living_grossSalary <- us_cost_of_living %>%
+      # filter by state from user input
+      filter(stateAbbr==state) %>%
+      # gross salary is calculated by working minimum wage 40 hours a week
+      # and 52 weeks in a year
+      mutate(grossSalary = minimumWage * 40 * 52)
+    return(us_cost_of_living_grossSalary)
+  }
+  else {
+    # throws an error if user does not input a valid state
+    print("Error: Please enter valid state abbreviations and check that
+          the letters are capitalized.")
+  }
+}
+
+
+#' A function that calculates the annual gross salary of a single person earning
+#' minimum wage at one or more states, given a vector of states as input.
+#'
+#'
+#' @import dplyr
 #' @import purrr
-#' @import utils
 #'
 #' @param states A list of states
 #'
-#' @return A tibble of taxes, average living costs, minimum wage, and gross salaries earning minimum wage
-#' per state
+#' @return A tibble of taxes, average living costs, minimum wage, and gross
+#' salaries earning minimum wage per state
 #'
 #' @export
 #'
@@ -21,10 +63,6 @@
 #' grossSalaryMultiple("MA")
 #'
 
-# define global variables
-globalVariables(c("minimumWage", "stateAbbr", "us_cost_of_living"))
-
-# function input takes in a list of states
 grossSalaryMultiple <- function(states) {
   # each state is passed to the grossSalary function to calculate gross salary and
   # iterated using map()
@@ -33,20 +71,4 @@ grossSalaryMultiple <- function(states) {
   bind_rows(state_rows)
 }
 
-# function does calculation
-grossSalary <- function(input) {
-  # checks if user inputs a valid state abbreviation
-    if (input %in% us_cost_of_living$stateAbbr) {
-      us_cost_of_living_grossSalary <- us_cost_of_living %>%
-        # filter by user input
-        filter(stateAbbr==input) %>%
-        # gross salary is calculated by working minimum wage 40 hours a week and 52 weeks in a year
-        mutate(grossSalary = minimumWage * 40 * 52)
-      return(us_cost_of_living_grossSalary)
-    }
-    else {
-      # throws an error if user does not input a valid state
-      print("Error: Please enter valid state abbreviations and check that the letters are capitalized.")
-    }
-}
 
