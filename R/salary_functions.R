c36044fc93ab01674d8c14e3a94f0cb9eb0d1a4f
PuppehMama <mtran01@smith.edu>
2022-12-01 17:51:30 -0500

    created 3 new functions, updated avg_col_multiple_states() function, added roxygen skeletons and comments, updated readme

--- R/salary_functions.R
+++ R/salary_functions.R
@@ -8,7 +8,7 @@ globalVariables(c("minimum_wage", "state_abbr", "us_minimum_wage"))
 #' A function that calculates the annual gross salary of a single person earning
 #' minimum wage at a specific state, given a state as input.
 #' `gross_salary` is an added column to the existing `us_minimum_wage` data set.
-#' This serves as a helper function to the following `gross_salary_multiple()`
+#' This serves as a helper function to the following `avg_col_multiple_states()`
 #' function.
 #'
 #' @import dplyr
@@ -16,8 +16,8 @@ globalVariables(c("minimum_wage", "state_abbr", "us_minimum_wage"))
 #'
 #' @param input_state A state name, abbreviated (e.g. MA)
 #'
-#' @return A tibble of taxes, average living costs, minimum wage, and gross
-#' salaries earning minimum wage per state
+#' @return A tibble of the state, minimum wage, and gross salary calculation
+#' earning minimum wage
 #'
 #' @export
 #'
@@ -43,44 +43,15 @@ gross_salary <- function(input_state) {
 }
 
 
-#' Function to calculate gross salaries across multiple states
-#'
-#' A function that calculates the annual gross salary of a single person earning
-#' minimum wage at one or more states, given a vector of states as input.
-#'
-#'
-#' @import dplyr
-#' @import purrr
-#' @param states A list of states
-#'
-#' @return A tibble of taxes, average living costs, minimum wage, and gross
-#' salaries earning minimum wage per state
-#'
-#' @export
-#'
-#' @examples
-#' gross_salary_multiple(c("MA","TX","IL","AL"))
-#' gross_salary_multiple("MA")
-#'
-
-gross_salary_multiple <- function(states) {
-  # each state is passed to the `gross_salary()` function to calculate gross salary
-  # and iterated using map()
-  state_rows <- map(states, gross_salary)
-  # separate tibbles are combined into one
-  state_rows <- bind_rows(state_rows)
-  return(state_rows)
-}
-
 # Define global variables
 #'
 #' @import utils
 globalVariables(c("min_income", "max_income", "fed_tax_rate", "us_federal_tax_rate"))
 
-#' Function to calculate tax owed by an individual based on their gross income
+#' Function to calculate the federal tax owed by an individual based on their gross income
 #'
 #' A function that calculates the amount of federal tax owed by an individual earning
-#' minimum wage at a specific state, given a state as input.
+#' minimum wage based on federal income tax brackets, given a state as input.
 #'
 #' @import dplyr
 #' @import purrr
@@ -88,8 +59,8 @@ globalVariables(c("min_income", "max_income", "fed_tax_rate", "us_federal_tax_ra
 #'
 #' @param input_state A state name, abbreviated (e.g. MA)
 #'
-#' @return A tibble of taxes, average living costs, minimum wage, and gross
-#' salaries earning minimum wage per state
+#' @return A tibble of the state, minimum wage, gross salary calculation
+#' earning minimum wage, and the federal tax owed based on income
 #'
 #' @export
 #'
@@ -99,11 +70,18 @@ globalVariables(c("min_income", "max_income", "fed_tax_rate", "us_federal_tax_ra
 #'
 
 federal_tax_owed <- function(input_state) {
+  # checks if user inputs a valid state abbreviation
+  if (input_state %in% us_minimum_wage$state_abbr) {
+  # the user's input is passed through the `gross_salary()` function
+  # the output is saved to state_row
   state_row <- gross_salary(input_state)
-  state_gross_salary <- state_row$gross_salary
-  if (!is.null(dim(state_row))) {
+    # subset the gross_salary column to perform calculations
+    state_gross_salary <- state_row$gross_salary
+    # series of if statements to match the state's gross salary to the correct income tax bracket
+    # example: the first tax bracket is between 0 and 10275 and 10% is taxable income
     if (state_gross_salary >= us_federal_tax_rate$min_income[1] & state_gross_salary <= us_federal_tax_rate$max_income[1]) {
       state_row <- state_row %>%
+        # calculate the federal tax owed by multiplying gross salary by the tax rate (and add any baseline numbers if necessary)
         mutate(federal_tax_owed = state_gross_salary * (us_federal_tax_rate$fed_tax_rate[1] / 100))
     } else if (state_gross_salary >= us_federal_tax_rate$min_income[2] & state_gross_salary <= us_federal_tax_rate$max_income[2]) {
       state_row <- state_row %>%
@@ -120,15 +98,180 @@ federal_tax_owed <- function(input_state) {
     } else if (state_gross_salary >= us_federal_tax_rate$min_income[6] & state_gross_salary <= us_federal_tax_rate$max_income[6]) {
       state_row <- state_row %>%
         mutate(federal_tax_owed = (state_gross_salary - 215950) * (us_federal_tax_rate$fed_tax_rate[6] / 100) + 49335.50)
-    } else { # state_gross_salary >= us_federal_tax_rate$min_income[7]
+    } else { # also means state_gross_salary >= us_federal_tax_rate$min_income[7]
       state_row <- state_row %>%
         mutate(federal_tax_owed = (state_gross_salary - 539900) * (us_federal_tax_rate$fed_tax_rate[7] / 100) + 162718)
     }
     return(state_row)
   }
   else {
-    print("Error")
+    # throws an error if user does not input a valid state
+    print("Error: Please enter valid state abbreviations and check that the letters are capitalized.")
+  }
+}
+
+# Define global variables
+#'
+#' @import utils
+globalVariables(c("us_minimum_wage", "federal_tax_owed" , "us_state_local_tax_rate", "state_local_tax_owed"))
+
+#' Function to calculate the state and local tax owed by an individual based on their gross income
+#'
+#' A function that calculates the amount of state and local tax owed by an individual earning
+#' minimum wage, given a state as input.
+#'
+#' @import dplyr
+#' @importFrom magrittr %>%
+#'
+#' @param input_state A state name, abbreviated (e.g. MA)
+#'
+#' @return A tibble of the state, minimum wage, gross salary calculation
+#' earning minimum wage, federal tax owed based on income, and state and local tax owed
+#' based on income
+#'
+#'
+#' @export
+#'
+#' @examples
+#' state_local_tax_owed("TX")
+#' state_local_tax_owed("MA")
+#'
+state_local_tax_owed <- function(input_state) {
+  # checks if user inputs a valid state abbreviation
+  if (input_state %in% us_minimum_wage$state_abbr) {
+    # the user's input is passed through the `federal_tax_owed()` function
+    # the output is saved to state_row
+  state_row <- federal_tax_owed(input_state)
+  us_state_local_tax_rate <- us_state_local_tax_rate %>%
+    # filter by state from user input
+    filter(state_abbr==input_state)
+  state_row <- state_row %>%
+    # state and local tax are calculated by multiplying the combined rate by the gross salary
+    mutate(state_local_tax_owed = (us_state_local_tax_rate$combined_tax_rate / 100) * gross_salary)
+  return(state_row)
+  } else {
+    # throws an error if user does not input a valid state
+    print("Error: Please enter valid state abbreviations and check that the letters are capitalized.")
+  }
+}
+
+# Define global variables
+#'
+#' @import utils
+globalVariables(c("us_minimum_wage", "federal_tax_owed" , "total_tax_owed", "state_local_tax_owed" , "net_salary"))
+
+#' Function to calculate the total tax owed and net salary of an individual based on their gross income
+#'
+#' A function that calculates the combined amount of federal, state, and local tax owed by an individual earning
+#' minimum wage, and their net salary, given a state as input.
+#'
+#' @import dplyr
+#' @importFrom magrittr %>%
+#'
+#' @param input_state A state name, abbreviated (e.g. MA)
+#'
+#' @return A tibble of the state, minimum wage, gross salary,
+#' total tax owed based on income, and net salary
+#'
+#'
+#' @export
+#'
+#' @examples
+#' net_salary("TX")
+#' net_salary("MA")
+#'
+net_salary <- function(input_state) {
+  # checks if user inputs a valid state abbreviation
+  if (input_state %in% us_minimum_wage$state_abbr) {
+    # the user's input is passed through the `state_local_tax_owed()` function
+    # the output is saved to state_row
+  state_row <- state_local_tax_owed(input_state)
+  state_row <- state_row %>%
+    # total tax owed is calculated by adding state, local, and federal tax amounts
+    mutate(total_tax_owed = state_local_tax_owed + federal_tax_owed) %>%
+    # net salary is calculated by finding the difference between gross salary and total tax owed
+    mutate(net_salary = gross_salary - total_tax_owed)
+  return(state_row)
+  } else {
+    # throws an error if user does not input a valid state
+    print("Error: Please enter valid state abbreviations and check that the letters are capitalized.")
+  }
+}
+
+# Define global variables
+#'
+#' @import utils
+globalVariables(c("us_minimum_wage", "net_salary" , "us_cost_of_living", "net_salary_avg_col_difference"))
+
+#' Function to calculate the difference between average cost of living and net salary
+#'
+#' A function that calculates the difference between the average costs of living (grocery, medical, housing, etc)
+#' by net salary, given a state as input.
+#'
+#' @import dplyr
+#' @importFrom magrittr %>%
+#'
+#' @param input_state A state name, abbreviated (e.g. MA)
+#'
+#' @return A tibble of the state, minimum wage, gross salary,
+#' minimum wage, total tax, net salary,
+#' and the difference between net salary and average cost of living
+#'
+#'
+#' @export
+#'
+#' @examples
+#' net_salary_avg_col_difference("TX")
+#' net_salary_avg_col_difference("MA")
+#'
+net_salary_avg_col_difference <- function(input_state) {
+  # checks if user inputs a valid state abbreviation
+  if (input_state %in% us_minimum_wage$state_abbr) {
+    # the user's input is passed through the `net_salary()` function
+    # the output is saved to state_row
+    state_row <- net_salary(input_state)
+    us_cost_of_living <- us_cost_of_living %>%
+      # filter by state from user input
+      filter(state_abbr==input_state)
+    state_row <- state_row %>%
+      # the average cost of living is subtracted by the net salary (the difference)
+      mutate(net_salary_avg_col_difference = net_salary - us_cost_of_living$total_avg_cost)
+    return(state_row)
+  } else {
+    # throws an error if user does not input a valid state
+    print("Error: Please enter valid state abbreviations and check that the letters are capitalized.")
   }
 }
 
 
+
+#' Function to calculate gross salary, tax, net salary, and difference between average cost of living
+#' and net salary across multiple states
+#'
+#' A function that calculates the annual gross salary, total tax (federal, state, local), net salary,
+#' and difference between average cost of living and net salary
+#' of a single person earning minimum wage at one or more states, given a vector of states as input.
+#'
+#'
+#' @import dplyr
+#' @import purrr
+#' @param states A list of states
+#'
+#' @return A tibble of minimum wage, gross salaries earning minimum wage, federal,
+#' state, local taxes, net salaries, and difference between average cost of living
+#' and net salary per state
+#'
+#' @export
+#'
+#' @examples
+#' avg_col_multiple_states(c("MA","TX","IL","AL"))
+#' avg_col_multiple_states("MA")
+#'
+
+avg_col_multiple_states <- function(states) {
+  # each state is passed to the `net_salary_avg_col_difference()` function and iterated using map()
+  state_rows <- map(states, net_salary_avg_col_difference)
+  # separate tibbles are combined into one
+  state_rows <- bind_rows(state_rows)
+  return(state_rows)
+}
