710f4dfccd571993e48050a55057b64a749e58a8
Thu Tran <tntran@smith.edu>
2022-12-13 23:48:43 -0500

    Make changes to pie chart

--- R/avg_col_breakdown_plot.R
+++ R/avg_col_breakdown_plot.R
@@ -2,7 +2,8 @@
 #'
 #' @import utils
 globalVariables(c("us_cost_of_living", "state_abbr", "state", "total_avg_cost",
-                  "t.avg_col_breakdown.", "living_cost", ""))
+                  "t.avg_col_breakdown.", "living_cost", "", "category", "cost",
+                  "cost_pct", "csum", "pos"))
 #' Function that plots the average living costs per state
 #'
 #' A function that plots the average cost of living for a single person earning
@@ -12,6 +13,9 @@ globalVariables(c("us_cost_of_living", "state_abbr", "state", "total_avg_cost",
 #' @import dplyr
 #' @importFrom magrittr %>%
 #' @import ggplot2
+#' @import tidyr
+#' @import ggrepel
+#' @import forcats
 #'
 #' @param input_state A state name, abbreviated (e.g. MA)
 #'
@@ -31,31 +35,43 @@ avg_col_breakdown_plot <- function(input_state) {
   avg_col_breakdown <- us_cost_of_living %>%
     filter(input_state==state_abbr)  %>%
     subset(select=-c(state, state_abbr, total_avg_cost))
-  table <- data.frame(t(avg_col_breakdown))
-  table$living_cost <- row.names(table)
-  rownames(table) <- NULL
 
-  # idea in progress: create a new column calculating corresponding percentages
-  # table$percentage <- round(100*t.avg_col_breakdown./sum(t.avg_col_breakdown./2))
+  # Convert df from wide to long format
+  table <- avg_col_breakdown %>%
+    pivot_longer(
+      everything(),
+      names_to = "category",
+      values_to = "cost"
+    ) %>%
+    mutate(cost_pct = round((cost / sum(cost)) * 100, digits = 2))
 
-  # View(table)
+  # Create a position table based on percentage for labeling
+  pct_pos_table <- table %>%
+    mutate(csum = rev(cumsum(rev(cost_pct))),
+           pos = cost_pct/2 + lead(csum, 1),
+           pos = if_else(is.na(pos), cost_pct/2, pos))
 
-  plot <- ggplot(table, aes(x="", y=t.avg_col_breakdown., fill=living_cost)) +
-    geom_bar(width = 1, stat = "identity") +
-    coord_polar("y", start=0) +
-    theme_void() +
-    scale_fill_brewer("Blues") +
+  # Create a plot title
+  full_state_name <- us_cost_of_living %>%
+    filter(input_state==state_abbr) %>%
+    select(state)
+  plot_title <- paste("Average cost of living breakdown in", full_state_name, "in 2022")
+
+  plot <- ggplot(table, aes(x="", y=cost_pct, fill=fct_inorder(category))) +
+    geom_col(width = 1, col = 1) +
+    coord_polar(theta = "y") +
+    scale_fill_brewer(palette="Pastel2") +
+
+    geom_label_repel(data = pct_pos_table,
+                     aes(y = pos, label = paste("$", cost, "\n(", cost_pct, "%)", sep="")),
+                     size = 3.5, nudge_x = 1, show.legend = FALSE) +
+    labs(title = plot_title) +
     guides(fill = guide_legend(title = "Type of Living Cost")) +
-    labs(title = "Average cost of living breakdown per state", subtitle = input_state) +
-    geom_text(aes(label = t.avg_col_breakdown.),
-              position = position_stack(vjust = 0.5), color = "black") +
+    theme_void()
 
-  ## second idea for percentages
-
-    # would have to import the scales package to use the percent() function
-
-    # geom_text(aes(y = t.avg_col_breakdown./6 + c(0, cumsum(t.avg_col_breakdown.)[-length(t.avg_col_breakdown.)]),
-    #               label = percent(t.avg_col_breakdown./100)), size=3.5)
+    # Add a plus sign at the end of the previous line if you want to de-comment this
+    # geom_text(aes(label = cost),
+    #           position = position_stack(vjust = 0.5), color = "black")
   return(plot)
 
 }
